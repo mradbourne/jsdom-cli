7353f038db379d8c615a494b12de9d1eec5cb6ef
Nicolas Lupien <niclupien@gmail.com>
2013-10-29 23:13:23 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+jsdom-cli
+=========
+
+A mini REPL based on jsdom.
+
+USAGE
+-----
+
+node jsdom-cli http://example.com
+
+API
+---
+
+### open(url)
+
+Open a new url
+
+### inject(filename)
+
+Inject javascript. Ex: inject('./jquery.js')
+
---
+++ index.js
@@ -0,0 +1,45 @@
+var readline = require('readline')
+  , jsdom = require('jsdom')
+  , vm = require('vm')
+  , fs = require('fs')
+  , sandbox = {console: console, setTimeout: setTimeout}
+
+var rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+sandbox.open = function(url) {
+  jsdom.env(url, function(err, _window) {
+    if (err) return console.log(err);
+    sandbox.window = _window;
+    sandbox.document = _window.document;
+  });
+};
+
+sandbox.inject = function(file) {
+  var text = fs.readFileSync(file, "utf-8");
+  sandbox.window.eval(text);
+};
+
+var execute = function(code) {
+  var script = vm.createScript(code);
+  try {
+    console.log(script.runInNewContext(sandbox));
+  } catch(error) {
+    console.log('ERROR: ' + error);
+  }
+}
+
+var prompt = function() {
+  rl.question("> ", function(line) {
+    execute(line);
+    prompt();
+  }); 
+};
+
+if ((sandbox.url = process.argv[2])) {
+  sandbox.open(sandbox.url);
+}
+
+prompt();
